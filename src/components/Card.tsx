9ab05e380ba846ac4482623ff016da377850e771
Asfand4321 <116038883+Asfand4321@users.noreply.github.com>
2025-11-16 11:12:18 +0000

    Delete src/components directory

--- src/components/Card.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Card({ className = '', children }: { className?: string; children: React.ReactNode }) {
-  return <div className={`card p-5 ${className}`}>{children}</div>
-}
